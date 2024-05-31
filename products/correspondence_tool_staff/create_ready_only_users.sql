ded89babf53ed1d558aac7bb73565db1ad397109
lucas.shaw <lucas.shaw@digital.justice.gov.uk>
2024-05-31 14:35:29 +0100

    new view for offender sar subject type rejected cases

--- products/correspondence_tool_staff/create_ready_only_users.sql
+++ products/correspondence_tool_staff/create_ready_only_users.sql
@@ -11,8 +11,8 @@ GRANT readaccess TO <user_name>;
 CREATE ROLE branson_read_access;
 GRANT USAGE ON SCHEMA public TO branson_read_access;
 GRANT SELECT ON cases_outcome_reasons, category_references, retention_schedules, tmp_partial_cases, data_requests, contacts,
-  offender_subject_type_volume_exclude_rejected_case_view, offender_sar_vetting_track_view, offender_data_requests_volume_view,
-  warehouse_case_report_for_london_disclosure_related, warehouse_case_report_for_offender_sar_related TO branson_read_access;
+  offender_subject_type_volume_exclude_rejected_case_view, offender_subject_type_volume_rejected_case_view, offender_sar_vetting_track_view,
+  offender_data_requests_volume_view, warehouse_case_report_for_london_disclosure_related, warehouse_case_report_for_offender_sar_related TO branson_read_access;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO branson_read_access;
 GRANT CONNECT ON DATABASE '<database name>' to branson_read_access;
 create user "<user_name>" with password '<password>';
